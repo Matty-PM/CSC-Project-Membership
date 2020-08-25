f6cb5e9b99692984c7a8cdd1b8ca4fd6483a2ebe
Matt Pearson <matt.pearson@titianinnovations.com>
2020-08-25 09:51:56 +0800

    Given up on Python xD

--- membershiptypecalc.py
+++ membershiptypecalc.py
@@ -1,6 +1,8 @@
+x = 'global'
+
 def membertype(membership_total):
     if membership_total > 0 and membership_total < 999:
-        print("Regular Membership")
+        print("x inside:", x)
 
     elif membership_total > 1000 and membership_total < 1199:
         print("Bronze Membership")
@@ -10,3 +12,6 @@ def membertype(membership_total):
 
     else:
         print("Gold Membership")
+
+membertype()
+print("x outside:", x)
