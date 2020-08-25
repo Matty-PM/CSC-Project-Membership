c20ffd3d710fa31f0bbf48cf9220817356d57cd9
Matty-PM <matt.pearson2021@icloud.com>
2020-08-25 20:06:12 +0800

    All Packages Available

--- packages.py
+++ packages.py
@@ -8,6 +8,62 @@ package1 = {
 package2 = {
   "package_id": 2,
   "Type": "Gym",
-  "Session_No": "10",
+  "Session_No": 10,
   "Cost": 100.00
+}
+
+package3 = {
+  "package_id": 3,
+  "Type": "Gym",
+  "Session_No": 20,
+  "Cost": 100.00
+}
+
+package4 = {
+  "package_id": 4,
+  "Type": "Gym",
+  "Session_No": 50,
+  "Cost": 450.00
+}
+
+package5 = {
+  "package_id": 5,
+  "Type": "Swim",
+  "Session_No": 10,
+  "Cost": 55.00
+}
+
+package6 = {
+  "package_id": 6,
+  "Type": "Swim",
+  "Session_No": 20,
+  "Cost": 104.50
+}
+
+package7 = {
+  "package_id": 7,
+  "Type": "Spin",
+  "Session_No": 10,
+  "Cost": 120.00
+}
+
+package8 = {
+  "package_id": 8,
+  "Type": "Spin",
+  "Session_No": 20,
+  "Cost": 228.00
+}
+
+package9 = {
+  "package_id": 9,
+  "Type": "Zumba",
+  "Session_No": 10,
+  "Cost": 120.00
+}
+
+package10 = {
+  "package_id": 10,
+  "Type": "Zumba",
+  "Session_No": 20,
+  "Cost": 228.00
 }
