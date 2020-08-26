e5b55538824ee78217352a69371e848173f308aa
Matt Pearson <matt.pearson@titianinnovations.com>
2020-08-26 11:46:06 +0800

    new more changes

--- main1.py
+++ main1.py
@@ -5,7 +5,7 @@ import membershipdiscountcalc
 
 var = "%"
 
-membershiptypecalc.membershiptype(totalannualamount.membership_total);
-membershipdiscountcalc.membershipdiscountammount(membershiptypecalc.membership_type);
+membershiptypecalc.membershiptype(totalannualamount.membership_total)
+membershipdiscountcalc.membershipdiscountammount(membershiptypecalc.membership_type)
 
-print("Customer Membership is", membershiptypecalc.membership_type, "and the customer is entitled to a", membershipdiscountcalc.membership_discount,var, "discount.");
+print("Customer Membership is", membershiptypecalc.membership_type, "and the customer is entitled to a", membershipdiscountcalc.membership_discount,var, "discount.")
